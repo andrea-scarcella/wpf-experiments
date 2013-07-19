a4f3606bcb3c6083cab4ae8853b5d51f09d18f63
Andrea Scarcella <andrea.scarcella.as@gmail.com>
2013-07-19 21:44:05 +0200

    Completed Ex 1 from 70511_chapter2_lesson2_practice1
    Defined custom command, defined input gestures

---
+++ 70511_chapter2_lesson2_practice1/Lesson 2/Lesson 2/CustomCommands.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+namespace Lesson_2
+{
+    public class CustomCommands
+    {
+        private static RoutedUICommand launch_command;
+        static CustomCommands()
+        {
+            InputGestureCollection collection = new InputGestureCollection();
+            collection.Add(new KeyGesture(Key.L, ModifierKeys.Control));
+            launch_command = new RoutedUICommand("Launch", "Launch", typeof(CustomCommands), collection);
+        }
+        public static RoutedUICommand Launch
+        {
+            get
+            {
+                return launch_command;
+            }
+        }
+    }
+}
